754ea4576649ee0755acce31b768d54ac129be8f
Jayna528 <72705761+Jayna528@users.noreply.github.com>
2020-11-30 18:46:12 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var bullet;
+var wall, thickness;
+var speed;
+var weight;
+
+
+function setup() {
+  createCanvas(1600,400);
+  thickness = random(22,83);
+  bullet = createSprite(70, 200, 50, 10);
+  wall = createSprite(1200, 200, thickness, height/2);
+  wall.shapeColor = rgb(80,80,80);
+  speed = random(223,321);
+  bullet.velocityX = speed;
+  weight = random(30,52);
+}
+
+function draw() {
+  if(hasColided(bullet,wall)){
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+
+    if(damage < 10)
+    {
+      wall.shapeColor = color(0,255,0);
+    }
+
+    if(damage > 10)
+    {
+      wall.shapeColor = color(255,0,0);
+    }
+  }
+
+
+  background(0);  
+  drawSprites();
+}
+function hasColided(lbullet,lwall){
+  bulletRightEdge = lbullet.x +lbullet.width;
+  wallLeftEdge = lwall.x
+  if(bulletRightEdge >= wallLeftEdge)
+  {
+    return true;
+  }
+    return false;
+}
